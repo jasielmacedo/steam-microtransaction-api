797981db23c7b1f4e10844f619b6855cd1bed552
Jazz Macedo <jasielmacedo@gmail.com>
2021-09-16 18:39:23 -0300

    fix(unity): change unity example to support api changes

--- examples/unity/http/HttpRequestArgs.cs
+++ examples/unity/http/HttpRequestArgs.cs
@@ -6,19 +6,12 @@ namespace Jazz.http
 {
     public class HttpRequestArgs 
     {
-        public Dictionary<string,string> data;
+        public string appId;
 
-        public HttpRequestArgs()
+        public HttpRequestArgs(){}
+        public HttpRequestArgs(string appId)
         {
-            data = new Dictionary<string, string>();
+            this.appId = appId;
         }
-
-        public HttpRequestArgs(Dictionary<string, string> requestData)
-        {
-            data = requestData;
-        }
-
-        public static explicit operator HttpRequestArgs(Dictionary<string,string> a){  return new HttpRequestArgs(a);  }
-        public static implicit operator Dictionary<string,string>(HttpRequestArgs a){ return a.data; }
     }
 }
