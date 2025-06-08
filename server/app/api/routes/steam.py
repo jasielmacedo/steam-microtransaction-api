e9fee0b21095e258cdd24822b73527d86d71fccd
Jazz Macedo <576436+jasielmacedo@users.noreply.github.com>
2025-06-07 20:05:56 -0400

    Add finalize and status endpoints

--- server/app/api/routes/steam.py
+++ server/app/api/routes/steam.py
@@ -4,6 +4,8 @@ from app.api.controllers.steam import (
     get_reliable_user_info,
     check_app_ownership,
     init_purchase,
+    finalize_purchase,
+    check_purchase_status,
 )
 
 # Router configuration
@@ -11,5 +13,7 @@ router = APIRouter()
 
 # Route registration
 router.add_api_route("/GetReliableUserInfo", get_reliable_user_info, methods=["POST"])
-router.add_api_route("/CheckAppOwnership", check_app_ownership, methods=["POST"])
+router.add_api_route("/CheckAppOwnership", check_app_ownership, methods=["POST"]) 
 router.add_api_route("/InitPurchase", init_purchase, methods=["POST"])
+router.add_api_route("/FinalizePurchase", finalize_purchase, methods=["POST"])
+router.add_api_route("/CheckPurchaseStatus", check_purchase_status, methods=["POST"])
